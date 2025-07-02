fa6442b4ab12f5e77c8031c49115d743a0a23372
Tsvetozara <tsvetozara.beneva@gmail.com>
2025-07-02 10:50:29 +0300

    LargestOf3Numbers

--- src/test/java/LargestOf3Numbers.java
+++ src/test/java/LargestOf3Numbers.java
@@ -1,2 +1,16 @@
 public class LargestOf3Numbers {
+    public static void main(String[] args) {
+        int a=10, b=20, c=30;
+
+        if(a>b && a>c){
+            System.out.println("a is the largest number");
+        }
+        else if(b>c && b>a)
+        {
+            System.out.println("b is the largest number");
+        }
+        else{
+            System.out.println("c is the largest number");
+        }
+    }
 }
