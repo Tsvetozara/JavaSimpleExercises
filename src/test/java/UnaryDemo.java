fbe9a2be00a48965dc4945ef263656e04c810e71
Tsvetozara <tsvetozara.beneva@gmail.com>
2025-07-02 11:12:15 +0300

    Other tests with exercises

---
+++ src/test/java/UnaryDemo.java
@@ -0,0 +1,22 @@
+class UnaryDemo {
+
+    public static void main(String[] args){
+        // result is now 1
+        int result = +1;
+        System.out.println(result);
+        // result is now 0
+        result--;
+        System.out.println(result);
+        // result is now 1
+        result++;
+        System.out.println(result);
+        // result is now -1
+        result = -result;
+        System.out.println(result);
+        boolean success = true;
+        // true
+        System.out.println(success);
+        // false
+        System.out.println(!success);
+    }
+}
