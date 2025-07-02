6959e2e6b2a4e36a8ba2ec36e47e90692952e0a6
Tsvetozara <tsvetozara.beneva@gmail.com>
2025-07-02 10:57:05 +0300

    New tests

---
+++ src/test/java/ComparisonDemo.java
@@ -0,0 +1,31 @@
+class ComparisonDemo {
+
+    public static void main(String[] args){
+        int value1 = 2;
+        int value2 = 2;
+
+        if(value1 == value2) {
+            System.out.println("value1 == value2");
+            System.out.println("qm liuti chushki");
+            System.out.println("neshto");
+        }
+
+        if(value1 != value2) {
+            System.out.println("value1 != value2");
+        }
+
+        if(value1 > value2) {
+            System.out.println("value1 > value2");
+        }
+
+        if(value1 < value2) {
+            System.out.println("value1 < value2");
+        }
+
+        if(value1 <= value2)
+            System.out.println("value1 <= value2");
+
+
+    }
+}
+
