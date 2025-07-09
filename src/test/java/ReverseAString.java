f59a7d7b894d94b8c2b40c41a87b13712ace394e
Tsvetozara <tsvetozara.beneva@gmail.com>
2025-07-09 10:23:21 +0300

    Reverse a string

---
+++ src/test/java/ReverseAString.java
@@ -0,0 +1,38 @@
+public class ReverseAString {
+
+    public static void main(String[] args) {
+
+        //Approach 1 - lenght(), charAt()
+
+        String s = "selenium";
+        String rev = "";
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            rev = rev + s.charAt(i);
+        }
+        System.out.println("Reverse string is: " + rev);
+
+
+        //Approach 2 - without using string methods
+
+        String str = "welocome";
+        String reverse = "";
+
+        char a[] = str.toCharArray();
+
+        for (int i = a.length - 1; i >= 0; i--)
+        {
+            reverse = reverse + a[i];
+        }
+        System.out.println("Reverse string is: " + reverse);
+
+        //Approach 3 - Using StringBuffer Class
+
+        //StringBuffer b = new StringBuffer("welcome");
+
+        //.out.println("Reverse string is: " + b.reverse());
+
+    }
+}
+
+
