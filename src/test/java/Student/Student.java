aec3c7726258098c0af14a1b31658a5ce8af0b87
Tsvetozara <tsvetozara.beneva@gmail.com>
2025-07-10 11:31:01 +0300

    Different ways to store data into variables

---
+++ src/test/java/Student/Student.java
@@ -0,0 +1,29 @@
+package Student;
+
+public class Student {
+
+    //class variables
+    int sid;
+    String sname;
+    char grade;
+
+    void printStudentData()
+    {
+        System.out.println(sid + "   " + sname + "   " + grade);
+    }
+
+    void setStudentData(int id, String name, char gr) // local variables
+    {
+        sid = id;
+        sname = name;
+        grade = gr;
+    }
+
+    Student(int id, String name, char gr)
+    {
+        sid = id;
+        sname = name;
+        grade = gr;
+    }
+
+}
---
+++ src/test/java/Student/StudentMain.java
@@ -0,0 +1,31 @@
+package Student;
+
+public class StudentMain {
+
+    public static void main(String[] args) {
+
+        //Different ways to store data into variables
+
+       // Student stu = new Student();
+
+        // 1) Using object reference variables to store the data
+
+       // stu.sid = 101;
+       // stu.sname = "Tsveti";
+       // stu.grade = 'A';
+
+       // stu.printStudentData();
+
+        // 2) Using method to store the data
+
+      //  stu.setStudentData(201, "Boris", 'A');
+       // stu.printStudentData();
+
+        // 3) Using constructor
+
+        Student stu = new Student(101, "Tsveti", 'A');
+        stu.printStudentData();
+
+    }
+
+}
