cf450c2a404ce912c56217fcb769abdf2ba1356d
Tsvetozara <tsvetozara.beneva@gmail.com>
2025-07-10 14:43:00 +0300

    Overloading main method

---
+++ src/test/java/This/ThisKeyword.java
@@ -0,0 +1,33 @@
+package This;
+
+public class ThisKeyword {
+
+    int x,y;  //class variables / instance variables
+
+   // ThisKeyword(int a, int b)
+  //  {
+   //     x=a;
+  //      y=b;
+  //  }
+
+    void setData(int a, int b)
+    {
+        x=a;
+        y=b;
+    }
+    void display()
+    {
+        System.out.println(x);
+        System.out.println(y);
+    }
+
+    public static void main(String[] args) {
+
+        //ThisKeyword th = new ThisKeyword(100, 200);
+
+        ThisKeyword th = new ThisKeyword();
+        th.setData(100, 200);
+        th.display();
+
+    }
+}
