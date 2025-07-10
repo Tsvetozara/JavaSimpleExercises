8f02f340a3132a2cf644a93281a2002a14b97657
Tsvetozara <tsvetozara.beneva@gmail.com>
2025-07-10 10:26:41 +0300

    Example for the 4 types of methods

---
+++ src/test/java/GreetingsExampleForTypesOfMethods/Greetings.java
@@ -0,0 +1,30 @@
+package GreetingsExampleForTypesOfMethods;
+
+public class Greetings {
+
+    // 1) Method with No params and No return value
+    void m1()
+    {
+        System.out.println("Hello....");
+    }
+
+    // 2) Method with No params and A return value
+    String m2()
+    {
+        return ("Hello how are you?");
+    }
+
+    // 3) Method takes params and has No return value
+
+    void m3(String name)
+    {
+        System.out.println("Hello " + name);
+    }
+
+    // 4) Method takes params and has A returns value
+
+    String m4(String name)
+    {
+        return("Hello " + name);
+    }
+}
---
+++ src/test/java/GreetingsExampleForTypesOfMethods/GreetingsMain.java
@@ -0,0 +1,24 @@
+package GreetingsExampleForTypesOfMethods;
+
+public class GreetingsMain {
+
+    public static void main(String[] args) {
+
+        Greetings gr = new Greetings();
+
+        gr.m1();
+
+        String s = gr.m2();
+        System.out.println(s);
+
+       // System.out.println(gr.m2());
+
+        gr.m3("John");
+
+        String var = gr.m4("David");
+        System.out.println(var);
+
+        System.out.println(gr.m4("Tsveti"));
+
+    }
+}
