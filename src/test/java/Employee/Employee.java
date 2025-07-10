8d66ff263cf999c6cd56772a0f99ffc4bdcdbe4f
Tsvetozara <tsvetozara.beneva@gmail.com>
2025-07-10 09:51:46 +0300

    Example for class

---
+++ src/test/java/Employee/Employee.java
@@ -0,0 +1,21 @@
+package Employee;
+
+public class Employee {
+
+    //Variables
+
+    int eid;
+    String ename;
+    String job;
+    int sal;
+
+    //Methods
+
+    void display()
+    {
+        System.out.println(eid);
+        System.out.println(ename);
+        System.out.println(job);
+        System.out.println(sal);
+    }
+}
---
+++ src/test/java/Employee/EmployeeMain.java
@@ -0,0 +1,25 @@
+package Employee;
+
+public class EmployeeMain {
+
+    public static void main(String[] args) {
+
+        Employee emp1 = new Employee(); //object 1
+
+        emp1.eid = 101;
+        emp1.sal = 4500;
+        emp1.ename = "Tsveti";
+        emp1.job = "Tester";
+        emp1.display();
+
+        // System.out.println(emp1.eid);
+
+        Employee emp2 = new Employee(); //object 2
+
+        emp2.eid = 102;
+        emp2.sal = 4500;
+        emp2.ename = "Tsvetozara";
+        emp2.job = "QA Tester";
+        emp2.display();
+    }
+}
