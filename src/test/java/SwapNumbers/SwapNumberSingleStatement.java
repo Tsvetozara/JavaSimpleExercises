3300c5004fea01056197e06d62ceabcb634b925c
Tsvetozara <tsvetozara.beneva@gmail.com>
2025-07-11 14:44:04 +0300

    Different approaches for swapping 2 numbers

--- src/test/java/SwapNumbers/SwapNumberSingleStatement.java
+++ src/test/java/SwapNumbers/SwapNumberSingleStatement.java
@@ -1,19 +1,15 @@
 package SwapNumbers;
 
-public class SwapNumbersWithoutThirdVariable {
+public class SwapNumberSingleStatement {
 
     public static void main(String[] args) {
-
-        // Used only if variables are not equal to 0
 
         int a = 10;
         int b = 20;
 
         System.out.println("Before swapping values are: " + a + " and " + b);
 
-        a = a + b;
-        b = a - b;
-        a = a - b;
+        b = a + b - (a=b);
 
         System.out.println("After swapping values are: " + a + " and " + b);
 
--- src/test/java/SwapNumbers/SwapNumbersWithMultiplicationAndDivision.java
+++ src/test/java/SwapNumbers/SwapNumbersWithMultiplicationAndDivision.java
@@ -1,22 +1,18 @@
 package SwapNumbers;
 
-public class SwapNumbersWithoutThirdVariable {
+public class SwapNumbersWithMultiplicationAndDivision {
 
     public static void main(String[] args) {
-
-        // Used only if variables are not equal to 0
 
         int a = 10;
         int b = 20;
 
         System.out.println("Before swapping values are: " + a + " and " + b);
 
-        a = a + b;
-        b = a - b;
-        a = a - b;
+        a = a*b;
+        b = a/b;
+        a = a/b;
 
         System.out.println("After swapping values are: " + a + " and " + b);
-
-
     }
 }
