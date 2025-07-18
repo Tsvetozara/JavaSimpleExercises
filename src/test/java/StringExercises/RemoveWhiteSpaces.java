57d300f392a3e75a07fe15ed33df10d83f0d2b55
Tsvetozara <tsvetozara.beneva@gmail.com>
2025-07-18 23:47:33 +0300

    Remove white spaces in a string

---
+++ src/test/java/StringExercises/RemoveWhiteSpaces.java
@@ -0,0 +1,15 @@
+package StringExercises;
+
+public class RemoveWhiteSpaces {
+
+    public static void main(String[] args) {
+
+        String str = "Java   programming    selenium    automation";
+
+        System.out.println("Before removing the white spaces: " + str);
+
+        str = str.replaceAll("\\s", "");
+
+        System.out.println("After removing the white spaces: " + str);
+    }
+}
